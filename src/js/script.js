1a02392f53aad350ffda29c042e7de9adeb23ff3
chomikoo <trzepla.szymon@gmail.com>
2018-10-31 14:24:18 +0100

    scss modules

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,18 @@
 	
 	console.log('Hello from script.js ');
 
+	const hamburger = document.getElementById('hamburger');
+	const menuList = document.getElementById('navbar-menu');
+
+
+	const toggleMenu = function(){
+		hamburger.classList.toggle('active');
+		menuList.classList.toggle('active');
+	}
+
+	hamburger.addEventListener('click', function() {
+		toggleMenu();
+	})
+
+
 })()
